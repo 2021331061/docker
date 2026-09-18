13df734af73e8ccf2d5f2faf7eadbe97dc366db3
2021331061 <omimahmud8@gmail.com>
2026-09-19 00:33:15 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+const port= process.env.PORT ?? 8080
+
+app.get("/", (req, res) => {
+  return res.send("Hello World!");
+
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    })
